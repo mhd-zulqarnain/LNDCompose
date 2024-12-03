6346233986abc517e6fff8fbd284a6ca2ca9c8fa
Zulqarnain <mhd.zeeq@gmail.com>
2024-12-03 11:04:33 +0500

    counter sample

--- app/src/main/java/com/learning/lndcompose/MainActivity.kt
+++ app/src/main/java/com/learning/lndcompose/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.learning.lndcompose.ui.theme.LNDComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +26,6 @@ class MainActivity : ComponentActivity() {
             LNDComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +35,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting( modifier: Modifier = Modifier) {
+    val count = remember {
+        mutableStateOf(0)
+    }
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("Counter :${count.value}")
+        ElevatedButton(onClick = {
+            count.value++
+        }) {
+            Text("Increment ")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LNDComposeTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
