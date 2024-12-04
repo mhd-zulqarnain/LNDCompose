33a93fe041d26d1c92e952458eb4de646ad88d89
Zulqarnain <mhd.zeeq@gmail.com>
2024-12-04 16:17:59 +0500

    radio button

--- app/src/main/java/com/learning/lndcompose/MainActivity.kt
+++ app/src/main/java/com/learning/lndcompose/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.learning.lndcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,19 +41,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting( modifier: Modifier = Modifier) {
-    val count = remember {
-        mutableStateOf(0)
+
+    var checked by remember {
+        mutableStateOf("option 1")
     }
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text("Counter :${count.value}")
-        ElevatedButton(onClick = {
-            count.value++
-        }) {
-            Text("Increment ")
-        }
+
+    Column {
+        RadioButtonText(
+            selected = checked == "option 1", {
+                checked = "option 1"
+            }, "option 1"
+
+        )
+        RadioButtonText(
+            selected = checked == "option 2", {
+                checked = "option 2"
+            }, "option 2"
+        )
     }
+
 }
 
+
+@Composable
+fun RadioButtonText(
+    selected: Boolean,
+    checked: () -> Unit,
+    label: String
+) {
+    Row (verticalAlignment = Alignment.CenterVertically , modifier = Modifier.padding(8.dp)){
+        RadioButton(
+            selected = selected, checked
+        )
+        Text(label)
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
