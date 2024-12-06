8399f64303d50faece9c995f1df21832d5c797c8
Zulqarnain <mhd.zeeq@gmail.com>
2024-12-06 10:44:04 +0500

    State hoisting

--- app/src/main/java/com/learning/lndcompose/MainActivity.kt
+++ app/src/main/java/com/learning/lndcompose/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.learning.lndcompose
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Button
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,8 +16,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.learning.lndcompose.ui.theme.LNDComposeTheme
@@ -41,41 +40,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting( modifier: Modifier = Modifier) {
-
-    var checked by remember {
-        mutableStateOf("option 1")
+    var progress by remember {
+        mutableStateOf(0.1f)
     }
-
     Column {
-        RadioButtonText(
-            selected = checked == "option 1", {
-                checked = "option 1"
-            }, "option 1"
+        LinearProgressIndicator(
+            progress = progress,
+            color = Color.Green,
+            trackColor = Color.Gray,
+            modifier = Modifier.padding(8.dp)
+        )
+        Button(
+            {
+                progress += 0.1f
 
-        )
-        RadioButtonText(
-            selected = checked == "option 2", {
-                checked = "option 2"
-            }, "option 2"
-        )
+                if (progress > 1&& progress!=1.0f)
+                    progress=0.0f
+            },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text("Increase progress")
+        }
     }
+
 
 }
 
 
-@Composable
-fun RadioButtonText(
-    selected: Boolean,
-    checked: () -> Unit,
-    label: String
-) {
-    Row (verticalAlignment = Alignment.CenterVertically , modifier = Modifier.padding(8.dp)){
-        RadioButton(
-            selected = selected, checked
-        )
-        Text(label)
-    }
-}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
