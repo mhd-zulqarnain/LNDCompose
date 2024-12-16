1bbe1aa2532d8962008ef6c246ffcf369574d780
Zulqarnain <mhd.zeeq@gmail.com>
2024-12-16 11:03:33 +0500

    lnd compose contraint layout

--- app/src/main/java/com/learning/lndcompose/MainActivity.kt
+++ app/src/main/java/com/learning/lndcompose/MainActivity.kt
@@ -5,13 +5,13 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Home
@@ -19,33 +19,22 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.learning.lndcompose.ui.theme.LNDComposeTheme
 
 
@@ -66,7 +55,7 @@ class MainActivity : ComponentActivity() {
                     }
 
                 ) { innerPadding ->
-                    NavigationHost(
+                    ConstraintLayoutScreen(
                         modifier = Modifier.padding(innerPadding)
 
                     )
@@ -77,75 +66,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun NavigationHost(modifier: Modifier = Modifier) {
-    val navController = rememberNavController()
-    NavHost(
-        modifier = modifier.padding(16.dp), navController = navController, startDestination = "first"
-    ) {
-        composable(route = "first") { FirstScreen(navController) }
-        composable(
-            //for optional argument we send by using ?
-            route = "second/{name}?age={age}",
-            arguments = listOf(
-                navArgument("name") {
-                    type = NavType.StringType
-                },
-                navArgument("age") {
-                    type = NavType.StringType
-                    nullable =true
-                }
-            )
-        ) { navBackStackEntry ->
-            SecondScreen(
-                navController,
-                navBackStackEntry.arguments?.getString("name").toString(),
-                navBackStackEntry.arguments?.getString("age").toString()
-            )
-        }
-    }
+fun ConstraintLayoutScreen(modifier: Modifier = Modifier) {
 
-}
+    ConstraintLayout(modifier = modifier.fillMaxSize()) {
+        val (box1, box2, text1) = createRefs()
 
-@Composable
-fun FirstScreen(navController: NavController) {
-    var name by remember { mutableStateOf("") }
-    var age by remember { mutableStateOf("") }
-    Column {
-        TextField(
-            value = name,
-            onValueChange = {
-                name = it
-            }, placeholder = {
-                Text("Enter the name")
-            })
-        Spacer(modifier = Modifier.height(4.dp))
-        TextField(
-            value = age,
-            onValueChange = {
-                age = it
-            }, placeholder = {
-                Text("Enter the age")
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Red)
+            .constrainAs(box1) {
+                top.linkTo(parent.top, margin = 100.dp)
+                start.linkTo(parent.start, margin = 100.dp)
             })
 
-        //for optional argument we send by using ?
-        Button(onClick = {
-            navController.navigate("second/$name?age=$age")
-        }) {
-            Text("Next Screen")
-        }
-    }
-}
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Gray)
+            .constrainAs(box2) {
+                top.linkTo(box1.bottom, margin = 100.dp)
+                start.linkTo(box1.end, margin = 30.dp)
+            })
 
-@Composable
-fun SecondScreen(navController: NavController, name: String, age: String) {
-    Column {
-        Text("2nd screen")
-        Text("Your name is $name and age is $age")
-        Button(onClick = {
-            navController.navigateUp()
-        }) {
-            Text("go back")
-        }
     }
 }
 
